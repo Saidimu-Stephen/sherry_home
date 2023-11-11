2ecea5f15da3879e957cb893f2bd4e7771abb608
stephen saidimu <stevensaidimu@gmail.com>
2023-11-11 15:00:10 +0300

    mongo database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 /** @format */
 
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
+
 app.listen(4000, () => {
   console.log("server listening at port 4000");
 });
