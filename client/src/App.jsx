46cde8762baa8cc18ec2bc1aa3b065fe6f7516a3
stephen saidimu <stevensaidimu@gmail.com>
2023-11-11 12:05:45 +0300

    added routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignOut'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 function App() {
   return (
-    <div className='text-red-500
-    '>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={< Signin/>} />
+         <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+         <Route path='/profile' element={<Profile />} />
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
